8abad73594b8f54dfef1e879c702acc68cb6bd52
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-10-14 16:02:09 +0700

    create index page

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$('.slider-one').slick({
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
+$('.slider-two').slick({
+    infinite: true,
+    dots: true,
+    slidesToScroll: 2,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
+$('.slider-three').slick({
+    infinite: true,
+    slidesToScroll: 2,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 5000,
+});
+$('.slider-four').slick({
+    infinite: true,
+    slidesToScroll: 2,
+    slidesToShow: 5,
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+});
+
